f055e1ddccf039097567170eabd9f8a258a1df39
吕浩 <1274714546@qq.com>
2022-10-25 16:58:06 +0800

    init

---
+++ ask.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    // 描述
+    type: 'input',
+    name: 'name',
+    message: 'project a name',
+  },
+  {
+    // 作者
+    type: 'input',
+    name: 'author',
+    message: 'author?',
+  },
+  {
+    // 描述
+    type: 'input',
+    name: 'description',
+    message: 'description?',
+  },
+]
---
+++ test.js
@@ -0,0 +1,9 @@
+// 测试文件
+
+let obj = {
+  name:"<%=name%>",
+  author:"<%=author%>",
+  description:"<%=description%>",
+  ss:"上面分别是项目名称 作者 描述"
+}
+
